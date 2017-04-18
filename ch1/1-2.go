7f0d131faa0a540c7f728c2a28bd876560002967
AdmiralAsshat <AsshatAdmiral@gmail.com>
2017-04-17 21:39:40 -0400

    Exercises 1-1 and 1-2. For real this time.

---
+++ ch1/1-2.go
@@ -0,0 +1,16 @@
+// Exercise 1.2: Modify the echo program to print the index
+// and value of each of its arguments, one per line.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s string
+	for i := 1; i < len(os.Args); i++ {
+		s = os.Args[i]
+		fmt.Println(i, ":", s)
+	}
+}
